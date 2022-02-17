d4b0421326d25a43f74602fb5fb3f95262a32ae6
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2022-02-17 14:48:21 -0600

    Adding DEL and PUT methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -11,15 +11,31 @@ items = []
 class Item(Resource):
 
     def get(self, name):
-        for item in items:
-            if name == item['name']:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: name == x['name'], items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        if next(filter(lambda x: name == x['name'], items), None):
+            return {'message': 'Item with name {} already exists'.format(name)}, 400
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}, 200
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: name == x['name'], items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item, 200
 
 
 class ItemList(Resource):
